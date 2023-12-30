9915a03cf081be89e31340ce2415c75880af1140
okenna10 <101345398+okenna10@users.noreply.github.com>
2023-12-30 00:44:51 -0500

    Add files via upload

---
+++ common/vcs_files.tcl
@@ -0,0 +1,47 @@
+
+proc get_memory_files {QSYS_SIMDIR} {
+  set memory_files [list]
+  return $memory_files
+}
+
+proc get_common_design_files {QSYS_SIMDIR} {
+  set design_files [dict create]
+  return $design_files
+}
+
+proc get_design_files {QSYS_SIMDIR} {
+  set design_files [dict create]
+  dict set design_files "fifo_fifo_1910_kp32l7y.v" "$QSYS_SIMDIR/fifo/fifo/fifo_1910/sim/fifo_fifo_1910_kp32l7y.v"
+  dict set design_files "fifo.v"                   "$QSYS_SIMDIR/fifo/fifo/sim/fifo.v"                            
+  return $design_files
+}
+
+proc get_elab_options {SIMULATOR_TOOL_BITNESS} {
+  set ELAB_OPTIONS ""
+  if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+  } else {
+  }
+  return $ELAB_OPTIONS
+}
+
+
+proc get_sim_options {SIMULATOR_TOOL_BITNESS} {
+  set SIM_OPTIONS ""
+  if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+  } else {
+  }
+  return $SIM_OPTIONS
+}
+
+
+proc get_env_variables {SIMULATOR_TOOL_BITNESS} {
+  set ENV_VARIABLES [dict create]
+  set LD_LIBRARY_PATH [dict create]
+  dict set ENV_VARIABLES "LD_LIBRARY_PATH" $LD_LIBRARY_PATH
+  if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+  } else {
+  }
+  return $ENV_VARIABLES
+}
+
+
